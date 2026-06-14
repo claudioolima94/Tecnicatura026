f57149d30f9d3fe77019614d65ab24e35ccd216c
claudio <claudio.olima94@gmial.com>
2026-06-14 17:19:54 -0300

    Avance carpeta JavaScript y Sintaxis js y Java

--- JavaScript/Leccion02/02-01-ConcatencionDeCadenas.js
+++ JavaScript/Leccion02/02-01-ConcatencionDeCadenas.js
@@ -13,3 +13,21 @@ console.log(juntos);
 
 nombre += apellido; //Tercera concatencion usando el operador simplificado
 console.log(nombre);
+
+let nombre2 = "Pedro";
+console.log(nombre2);
+
+const apellido2 = "Lepes";
+//apellido2 = "Peres"; una constante no puede ser modificada
+console.log(apellido2)
+
+let x, y; //Se puede crear varias variables dentro de una misma linea
+x = 17, y = 21; //Se puede hacer asignacion de varias variables dentro de la misma linea
+let z = x + y;
+console.log(z);
+
+let _1num = 31; //No utilizar numeros para iniciar el nombre de una variable
+let rompiedno = "rompe"; //No utilizar paralabras reservadas para variables
+
+console.log(_1num);
+console.log(rompiedno);
---
+++ JavaScript/Leccion03/03-01-OperadoresJavaScript.js
@@ -0,0 +1,29 @@
+//Ejercicio para encontrar numeros pares e impares
+let parInpar = 10
+if(parInpar % 2 == 0) {
+    console.log("Es un numero PAR");
+
+}
+else{
+    console.log("Es un numero IMPAR");
+}
+
+//Ejercicio: es mayor de edad
+let edad = 18, adulto = 18;
+if( edad >= adulto){
+    console.log("usted es una persona adulta");
+
+}
+else{
+    console.log("Usted es una persona menor de edad")
+}
+
+//Ejercicion: Dentro de un rango
+let dentroRango = 5; //Aqui vamos a ir cambiando el valor
+let valMin = 0, valMax = 10;
+if( dentroRango >= valMin && dentroRango <= valMax){
+    console.log('Esta dentro del rango establecido')
+}
+else{
+    console.log('Esta fuera del rango establecido')
+}
