8ab11fac7bb9d561e462e3fb3642e30fe1393b9c
claudio <claudio.olima94@gmial.com>
2026-08-18 17:00:18 -0300

    Clase en Java cpn brack, continue y labels

--- Java/SegundoSemestre/CicloWhile/src/ciclowhile/CicloWhile.java
+++ Java/SegundoSemestre/CicloWhile/src/ciclowhile/CicloWhile.java
@@ -14,5 +14,21 @@ public class CicloWhile {
          System.out.println("contador = " + contador);
          contador++;
     }while(contador <= 7);
+    
+     for(var contando = 0; contando < 7; contando++) {
+         if(contando % 2 == 0){
+              System.out.println("contando = " + contando);
+              break;
+         }
+       
+         
+     }
+     inicio:
+       for(var contando = 0; contando < 7; contando++) {
+         if(contando % 2 != 0){
+             continue inicio;
+         }
+           System.out.println("contando = " + contando);
+     }
   } 
 }
